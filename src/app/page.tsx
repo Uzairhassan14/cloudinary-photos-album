b76b5601a3895d1cdea374404cfe75dff1273543
Uzairhassan14 <uh369682@gmail.com>
2023-09-02 16:07:05 +0500

    updated type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CldUploadButton
-        onUpload={(result: UploadReslut) => {
+        onUpload={(result: any) => {
           setImageId(result.info.public_id);
         }}
         uploadPreset="nkxtlg58"
